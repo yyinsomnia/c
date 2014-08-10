8c9fe68ad3889e1088a16e05198120a7f9a74ace
yyinsomnia <yyinsomnia@gmail.com>
2014-08-11 00:43:57 +0800

    mac with llvm is better for error report

---
+++ listing_5_7_b.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLINES 5000 //进行排序的最大文本行数
+
+char *lineptr[MAXLINES];  //指向问本行的指针数组
+
+int readlines(char *lineptr[], int nlines);
+void writelines(char *lineptr[], int nlines);
+
+void qsort(char *lineptr[], int left, int right);
+
+int main()
+{
+	int nlines; //读取的输入行数目
+	if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
+		qsort(lineptr, 0, nlines - 1);
+		writelines(lineptr, nlines);
+		return 0;
+	} else {
+		printf("error: input too big to sort\n");
+		return 1;
+	}
+}
+
+#define MAXLEN 1000
+int getline_my(char *, int);
+//Both getline() and getdelim() were originally GNU extensions. They were standardized in POSIX.1-2008.
+//char *alloc(int);
+
+int readlines(char *lineptr[], int maxlines)
+{
+	int len, nlines;
+	char *p, line[MAXLEN];
+
+	nlines = 0;
+	while ((len = getline_my(line, MAXLEN)) > 0) {
+		if (nlines >= maxlines || (p = (char*) malloc(len)) == NULL) {
+			return -1;
+		} else {
+			line[len - 1] = '\0';
+			strcpy(p, line);
+			lineptr[nlines++] = p;
+		}
+	}
+	return nlines;
+}
+
+void writelines(char *lineptr[], int nlines)
+{
+	int i;
+	for (i = 0; i < nlines; i++)
+		printf("%s\n", lineptr[i]);
+}
+
+void qsort(char *v[], int left, int right)
+{
+	int i, last;
+	void swap(char *v[], int i, int j);
+	if (left >= right)
+		return;
+	swap(v, left, (left + right) / 2);
+	last = left;
+	for (i = left + 1; i <= right; i++) {
+		if (strcmp(v[i], v[left]) < 0)
+			swap(v, ++last, i);
+	}
+	swap(v, left, last);
+	qsort(v, left, last - 1);
+	qsort(v, last + 1, right);
+}
+
+void swap(char *v[], int i, int j)
+{
+	char *temp;
+	temp = v[i];
+	v[i] = v[j];
+	v[j] = temp;
+}
+
+int getline_my(char s[], int lim)
+{
+	int c, i;
+	i = 0;
+	while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
+		s[i++] = c;
+	if (c == '\n')
+		s[i++] = c;
+	s[i] = '\0';
+	return i;
+}
