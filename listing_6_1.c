3ece975458249a46ab85f0409d7af3a61a86bb23
yyinsomnia <yyinsomnia@gmail.com>
2015-04-06 16:11:21 +0800

    finished

--- listing_6_1.c
+++ listing_6_1.c
@@ -3,9 +3,9 @@
 #include <string.h>
 
 #define MAXWORD 100
+#define NKEYS 32
 
 int getword(char *, int);
-int binsearch(char *, struct key *, int);
 
 struct key {
 	char *word;
@@ -18,12 +18,35 @@ struct key {
 	{"const", 0},
 	{"continue", 0},
 	{"default", 0},
-	/* ... */
+	{"do", 0},
+	{"double", 0},
+	{"else", 0},
+	{"enum", 0},
+	{"extern", 0},
+	{"float", 0},
+	{"for", 0},
+	{"goto", 0},
+	{"if", 0},
+	{"int", 0},
+	{"long", 0},
+	{"register", 0},
+	{"return", 0},
+	{"short", 0},
+	{"signed", 0},
+	{"sizeof", 0},
+	{"static", 0},
+	{"struct", 0},
+	{"switch", 0},
+	{"typedef", 0},
+	{"union", 0},
 	{"unsigned", 0},
 	{"void", 0},
 	{"volatile", 0},
 	{"while", 0}
 };
+
+int binsearch(char * word, struct key tab[], int n);
+
 int main()
 {
 	int n;
@@ -35,7 +58,7 @@ int main()
 				keytab[n].count++;
 	for (n = 0; n < NKEYS; n++)
 		if (keytab[n].count > 0)
-			printf("%4d %s\n", keytab[m].count, keytab[n].word);
+			printf("%4d %s\n", keytab[n].count, keytab[n].word);
 
 	return 0;
 }
@@ -61,7 +84,7 @@ int binsearch(char *word, struct key tab[], int n)
 
 int getword(char *word, int lim)
 {
-	int c;
+	int c, d, former, later;
 	int getch(void);
 	void ungetch(int);
 	char *w = word;
@@ -70,14 +93,35 @@ int getword(char *word, int lim)
 		;
 	if (c != EOF)
 		*w++ = c;
-	if (!isalpha(c)) {
-		*w = '\0';
-		return c;
-	}
-	for ( ; --lim > 0; w++) {
-		if (!isalnum(*w = getch())) {
-			ungetch(*w);
-			break;
+	if (isalpha(c) || c == '_' || c == '#') {
+		for ( ; --lim > 0; w++) {
+			if (!isalnum(*w = getch())) {
+				ungetch(*w);
+				break;
+			}
+		}
+	} else if (c == '\'' || c == '"') {
+		former = c;
+		while ((later = getch()) != EOF) {
+			if (later == c && former != '\'')
+				break;
+			former = later;
+		}
+	} else if (c == '/') {
+		if ((d = getch()) == '*') {
+			while ((c = getch()) != EOF) {
+				if (c == '*') {
+					if ((c = getch()) == '/')
+						break;
+					else 
+						ungetch(c);
+				}
+			}
+		} else if (d == '/') {
+			while ((c = getch()) != '\n') //TODO
+				;
+		} else {
+			ungetch(d);
 		}
 	}
 	*w = '\0';
